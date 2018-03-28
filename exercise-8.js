3a680236a0b50df258cc7846595a62a62bfb7d90
Dwiky Erlangga <dwikyerl@gmail.com>
2018-03-28 11:15:35 +0700

    Finished exercise 8 : Mengurutkan Abjad

---
+++ exercise-8.js
@@ -0,0 +1,31 @@
+/*
+  Logic Challenge - Mengurutkan Abjad
+*/
+
+function urutkanAbjad(str) {
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr.join('');
+}
+
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
+
+// Short Solution
+
+// function urutkanAbjad(str) {
+//   return str.split('').sort((a, b) => a > b).join('');
+// }
