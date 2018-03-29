b57223ac84a7bd714b0b3390ace5482d64cf9d6d
Dwiky Erlangga <dwikyerl@gmail.com>
2018-03-29 09:30:03 +0700

    Finished exercise 18 : Kali Terus Rekursif

---
+++ exercise-18.js
@@ -0,0 +1,20 @@
+/*
+  Logic Challenge - Kali Terus (Rekursif)
+*/
+
+function kaliTerusRekursif(angka) {
+  let str = "" + angka;
+
+  if (str.length === 1) return angka;
+
+  return kaliTerusRekursif(
+    parseInt(str[0]) * kaliTerusRekursif(parseInt(str.slice(1)))
+  );
+}
+
+// TEST CASES
+console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(1231)); // 6
