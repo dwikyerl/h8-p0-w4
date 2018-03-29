bdcaf5e7e8ccf2db9ea6444ff34f31cc7cb0a14e
Dwiky Erlangga <dwikyerl@gmail.com>
2018-03-29 08:26:45 +0700

    Finished exercise 15 : Password Generator

---
+++ exercise-15.js
@@ -0,0 +1,57 @@
+/*
+  Logic Challenge - Password Generator
+*/
+
+function changeVocals(str) {
+  const table = {
+    a: "b",
+    i: "j",
+    u: "v",
+    e: "f",
+    o: "p",
+    A: "B",
+    B: "J",
+    U: "V",
+    E: "F",
+    O: "P"
+  };
+
+  return str.replace(/[aiueo]/gi, match => table[match]);
+}
+
+function reverseWord(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+function setLowerUpperCase(str) {
+  let newStr = "";
+
+  for (let char of str) {
+    if (/[A-Z]/.test(char)) newStr += char.toLowerCase();
+    else newStr += char.toUpperCase();
+  }
+
+  return newStr;
+}
+
+function removeSpaces(str) {
+  return str.replace(/\s/g, "");
+}
+
+function passwordGenerator(name) {
+  if (name.length < 5) return "Minimal karakter yang diinputkan adalah 5 karakter";
+
+  const process1 = changeVocals(name);
+  const process2 = reverseWord(process1);
+  const process3 = setLowerUpperCase(process2);
+  const process4 = removeSpaces(process3);
+  return process4;
+}
+
+console.log(passwordGenerator("Sergei Dragunov")); // 'VPNVGBRdJFGRFs'
+console.log(passwordGenerator('Dimitri Wahyudiputra')); // 'BRTVPJDVYHBwJRTJMJd'
+console.log(passwordGenerator('Alexei')); // 'JFXFLb'
+console.log(passwordGenerator('Alex')); // 'Minimal karakter yang diinputkan adalah 5 karakter'
